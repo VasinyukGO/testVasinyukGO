3ee084d038a1e01cff9f5a18f28ffcecf73bc75a
Grigory <Neillias@users.noreply.github.com>
2020-02-16 14:09:24 +0300

    mobile version added

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,7 @@
+import mobileButton from '../mobile/button/mob-button';
 import mainNavButton from '../common.blocks/main-nav/main-nav';
 import headerButton from "../common.blocks/button/button";
 
+mobileButton();
 mainNavButton();
 headerButton();
---
+++ src/mobile/button/mob-button.js
@@ -0,0 +1,38 @@
+export default function mobileButton () {
+    document.addEventListener('click', function(e) {
+        if (e.target && e.target.className == 'mob-button' || e.target.parentNode.className == 'mob-button') {
+            
+            var thisButton = e.target;
+
+            if (e.target.className !== 'mob-button'){
+                thisButton = e.target.parentNode;
+            }
+
+            var thisToggleIcon = thisButton.querySelector('.mob-button__arrow');
+            var thisArticle = thisButton.nextElementSibling;
+            var allArticlesArray = thisButton.parentNode.querySelectorAll('.mob-article');
+            var allToggleIconsArray = thisButton.parentNode.querySelectorAll('.mob-button__arrow');
+
+            
+
+            if (thisArticle.style.display != 'block') {
+                for (let i = 0; i < allArticlesArray.length; i++) {
+                    allArticlesArray[i].style.display = 'none';        
+                }
+
+                for (let h = 0; h < allArticlesArray.length; h++) {
+                    allToggleIconsArray[h].style.transform = 'rotate('+0+'deg)';        
+                }
+                
+                thisArticle.style.display = 'block';
+                thisToggleIcon.style.transform = 'rotate('+180+'deg)';
+                document.querySelector('.body-background').style.backgroundImage = 'none';                
+
+            } else {
+                thisArticle.style.display = 'none'
+                thisToggleIcon.style.transform = 'rotate('+0+'deg)';
+                document.querySelector('.body-background').style.backgroundImage = 'url(/bg1.fc5ebf54.png)';
+            }
+        }
+    });
+}
