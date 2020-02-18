1ff2fdaa5d7ee6f8fe746cf6a29baf64a2eb94a2
Grigory <Neillias@users.noreply.github.com>
2020-02-18 14:48:44 +0300

    gh-pages added

--- src/js/main.js
+++
@@ -1,7 +0,0 @@
-import mobileButton from '../mobile/button/mob-button';
-import mainNavButton from '../common.blocks/main-nav/main-nav';
-import headerButton from "../common.blocks/button/button";
-
-mobileButton();
-mainNavButton();
-headerButton();
